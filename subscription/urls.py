150b32aeca0fca6791e1ae79766fa6b1461b9f46
Caio di Felice Cunha <caiofcunha@hotmail.com>
2024-11-26 08:18:00 -0800

    A lot of front end.
    Styling the home page better, including hiperlinks and colors, and sections and containers....
    
    register page is better looking now, also handing better the 'already registered' error
    
    register confirmed got another look as well

--- subscription/urls.py
+++ subscription/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.subscription, name='subscription'),
+    path('', views.index, name='index'),
+    path('register/', views.subscription, name='subscription'),
     path('aplicationprocess/', views.aplicationprocess, name='aplicationprocess'),
 ]
--- subscription/views.py
+++ subscription/views.py
@@ -7,20 +7,31 @@ import os
 from PIL import Image, ImageDraw, ImageFont
 from hashlib import sha256
 from .tasks import create_invitation
+from django.db import IntegrityError
 
 
 def subscription(request):
     return render(request, 'subscription.html')
 
 def aplicationprocess(request):
-
     name = request.POST.get('name')
     email = request.POST.get('email')
 
-    token = create_invitation.delay(name, email)
+    # Check if email already exists
+    if Person.objects.filter(email=email).exists():
+        return render(request, 'subscription.html', {
+            'error': 'This email is already registered for the event.',
+            'name': name,  # Return the name to preserve the form data
+            'email': email  # Return the email to preserve the form data
+        })
 
+    # If email doesn't exist, proceed with registration
     person = Person(name=name, email=email)
     person.save()
-
+    token = create_invitation.delay(name, email)
+    
     return render(request, 'register_confirmed.html', {'token': token})
+
+def index(request):
+    return render(request, 'index.html')
 
