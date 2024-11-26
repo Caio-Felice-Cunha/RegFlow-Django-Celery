2814f1b83f7433398e0a252147f06e96d6abab55
Caio di Felice Cunha <caiofcunha@hotmail.com>
2024-11-25 23:23:09 -0800

    create the send_email

--- subscription/views.py
+++ subscription/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Person
+from django.core.mail import send_mail
 
 def subscription(request):
     return render(request, 'subscription.html')
@@ -11,6 +12,13 @@ def aplicationprocess(request):
 
     person = Person(name=name, email=email)
     person.save()
+
+    send_mail(
+        'IASUMMIT | Subscription Received',
+        'Your application has been received. Thank you for your interest in IASUMMIT!',
+        'caiofcunha@hotmail.com',
+        recipient_list=[email]
+    )
 
     return HttpResponse(f"Name: {name}, Email: {email}")
 
